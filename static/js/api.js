d46e09574f32b43ecb12fa970852bfc4a3182d0f
AbrarZaved <abrarzaved2002@gmail.com>
2025-07-06 19:09:40 +0600

    Refactor CSRF token retrieval in csrfFetch to use getCSRFToken function

--- static/js/api.js
+++ static/js/api.js
@@ -1,11 +1,18 @@
 import { getCookie } from "/static/js/utils.js";
-
+function getCSRFToken() {
+  const cookies = document.cookie.split(";");
+  for (let cookie of cookies) {
+    const [name, value] = cookie.trim().split("=");
+    if (name === "csrftoken") return decodeURIComponent(value);
+  }
+  return null;
+}
 export async function csrfFetch(url, options = {}) {
   const csrftoken = getCookie("csrftoken");
 
   const defaultHeaders = {
     "Content-Type": "application/json",
-    "X-CSRFToken": csrftoken,
+    "X-CSRFToken": getCSRFToken(),
   };
 
   options.headers = {
