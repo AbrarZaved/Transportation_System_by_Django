b61b2bfb28f3ac7a7fbf2ace01ce42caecee9274
AbrarZaved <abrarzaved2002@gmail.com>
2025-07-19 23:44:06 +0600

    Add subscription page and update navigation links for improved user access

--- transport_manager/views.py
+++ transport_manager/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def subscription(request):
+    # This view will handle the subscription logic
+    return render(request, 'transport_manager/subscription.html')
